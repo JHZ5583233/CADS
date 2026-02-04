31b16d1b0bee1a0235cf05d4dfb710902eb1f3ba
JHZ5583233 <jiahoa.zhang@gmail.com>
2026-02-04 16:50:34 +0100

    made undo?

--- assignment_1/texteditor/TextEditor.c
+++ assignment_1/texteditor/TextEditor.c
@@ -6,6 +6,7 @@
 
 #define TEXT_EDITOR_INITIAL_CAPACITY 10
 
+
 TextEditor* createTextEditor(void) {
   TextEditor *editor = malloc(sizeof(*editor));
   editor->text = malloc(TEXT_EDITOR_INITIAL_CAPACITY * sizeof(*editor->text));
@@ -13,6 +14,7 @@ TextEditor* createTextEditor(void) {
 
   editor->length = 0;
   editor->capacity = TEXT_EDITOR_INITIAL_CAPACITY;
+  editor->action_history = newStack(TEXT_EDITOR_INITIAL_CAPACITY);
   return editor;
 }
 
@@ -23,20 +25,38 @@ void insertCharacter(TextEditor *editor, int pos, char character) {
     }
     editor->text[pos] = character;
     editor->length += 1;
+
+    EditOperation current_action = {INSERT, character, pos};
+    push(current_action, &editor->action_history);
 }
 
 void deleteCharacter(TextEditor *editor, int pos) {
   // Implement the delete operation
+    char deleted_char;
+    deleted_char = editor->text[pos];
 
     for (int i =pos;i<editor->length;i++) {
         editor->text[i] = editor->text[i+1];
     }
 
     editor->length -= 1;
+
+    EditOperation current_action = {DELETE, deleted_char, pos};
+    push(current_action, &editor->action_history);
 }
 
 void undo(TextEditor *editor) {
   // Implement the undo operation
+    EditOperation last_action;
+    last_action = pop(&editor->action_history);
+
+    if (last_action.type == INSERT) {
+        deleteCharacter(editor, last_action.position);
+    }
+    else if (last_action.type == DELETE)
+    {
+        insertCharacter(editor, last_action.position, last_action.character);
+    }
 }
 
 void redo(TextEditor *editor) {
@@ -46,6 +66,8 @@ void redo(TextEditor *editor) {
 void destroyTextEditor(TextEditor *editor) {
   // Don't forget to free!
     free(editor->text);
+    freeStack(editor->action_history);
+    free(editor);
 }
 
 void printText(TextEditor *editor) {
--- assignment_1/texteditor/TextEditor.h
+++ assignment_1/texteditor/TextEditor.h
@@ -1,8 +1,11 @@
 #ifndef TEXTEDITOR_H
 #define TEXTEDITOR_H
 
+#include "LibStack.h"
+
 typedef struct TextEditor {
   char *text;
+  Stack action_history;
   int length;
   int capacity;
 } TextEditor;
