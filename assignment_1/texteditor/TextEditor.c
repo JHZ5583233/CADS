aaf16db9886a19b9b063d9248e7c420e4463c53b
JHZ5583233 <jiahoa.zhang@gmail.com>
2026-02-04 17:06:20 +0100

    it grows

--- assignment_1/texteditor/TextEditor.c
+++ assignment_1/texteditor/TextEditor.c
@@ -26,6 +26,13 @@ void insertCharacter(TextEditor *editor, int pos, char character) {
     editor->text[pos] = character;
     editor->length += 1;
 
+    if (editor->length == editor->capacity) {
+      int new_capacity;
+      new_capacity = 2 * editor->capacity;
+      editor->capacity = new_capacity;
+      editor->text = realloc(editor->text, new_capacity * sizeof(*editor->text));
+    }
+
     EditOperation current_action = {INSERT, character, pos};
     push(current_action, &editor->action_history);
 }
